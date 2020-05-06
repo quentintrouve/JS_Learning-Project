be00afb1ed3061a0c3925f933f73041fa33cc6ff
quentintrouve <trouve.quentin@gmail.com>
2020-05-07 00:20:02 +0200

    template mvc

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,44 @@
-console.log("coucou");
+document.addEventListener("DOMContentLoaded", function () {
+  class Model {
+    constructor() {
+      this.pages = [
+        {
+          title: "Hompage",
+          url: "/",
+          background: "green",
+        },
+        {
+          title: "Contact",
+          url: "/contact",
+          background: "red",
+        },
+      ];
+    }
+
+    getPage(url) {
+      return this.pages.find((page) => page.url == url);
+    }
+  }
+
+  function controller() {
+    const currentUrl = "/";
+
+    const model = new Model();
+    const currentPage = model.getPage(currentUrl);
+  }
+
+  class View {
+    constructor(currentPage) {
+      this.container = document.querySelector(".container");
+
+      this.container.style.background = currentPage.background;
+      this.updateHtml(currentPage.title);
+    }
+
+    updateHtml(html) {
+      this.container.innerHTML = html;
+    }
+  }
+
+  controller();
+});
